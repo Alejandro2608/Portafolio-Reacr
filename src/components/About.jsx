0a9c9e26264aefc5ee080267163d85e3c3053962
Alejandro Ortega <alejo.ortegaa@gmail.com>
2019-07-18 21:27:59 -0500

    Actualización

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+/*
+const About= function() {
+    return <h1>Hola mundo Geek</h1>
+}
+const AboutDos = () => {
+    return <h1>Hola mundo Geek 2</h1>
+}
+*/
+//retornar de manera explicita
+const About = () => (<h1>Hola mundo Geek</h1>);
+
+export default About;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; //renderizar paginas en pantalla
+import About from './components/About';
+
+//Qué voy a renderizar (el componente, dónde lo voy a renderizar)
+ReactDOM.render(<About/>, document.getElementById('app'));
